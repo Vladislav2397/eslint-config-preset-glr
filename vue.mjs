80507d9480c63f59d7c58d514440cc0712fa480e
Vladislav <53734286+Vladislav2397@users.noreply.github.com>
2025-03-28 09:13:09 +0000

    Cleared vue config

--- vue.mjs
+++ vue.mjs
@@ -1,6 +1,3 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -8,21 +5,9 @@ export default [
     {
         files: ["**/*.{js,mjs,cjs,ts,vue}"]
     },
-    {
-        languageOptions: {
-            globals: globals.browser
-        }
-    },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
     ...pluginVue.configs["flat/essential"],
     {
         files: ['**/*.vue'],
-        languageOptions: {
-            parserOptions: {
-                parser: tseslint.parser
-            }
-        },
         rules: {
             'vue/multi-word-component-names': 'off',
             'vue/attribute-hyphenation': ['warn', 'never'],
@@ -34,6 +19,5 @@ export default [
             'vue/no-v-text-v-html-on-component': 'off',
             'vue/no-v-html': 'off',
         },
-        
     },
 ]
